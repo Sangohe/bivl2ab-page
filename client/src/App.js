5264ff748af4f13923122ab3d32f27486652a952
estudiantepythonudi <ramaya.udi@outlook.com>
2019-05-30 00:09:46 -0500

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import AppBar from './navbar/AppBar'
+import Components from "views/Components/Components.jsx";
+import LandingPage from "views/LandingPage/LandingPage.jsx";
+import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
+import LoginPage from "views/LoginPage/LoginPage.jsx";
+import { createBrowserHistory } from "history";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
+import "assets/scss/material-kit-react.scss?v=1.4.0";
+const hist = createBrowserHistory();
 function App() {
   return (
-    <div className="App">
-      <AppBar></AppBar>
-    </div>
+    <Router history={hist}>
+      <Switch>
+        <Route path="/landing-page" component={LandingPage} />
+        <Route path="/profile-page" component={ProfilePage} />
+        <Route path="/login-page" component={LoginPage} />
+        <Route path="/" component={Components} />
+      </Switch>
+    </Router>
   );
 }
 
